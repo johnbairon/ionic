0aa570612ab064d990ce19da56f5bded8f5f50eb
johnba <johnbairon.londo45@gmail.com>
2015-12-11 14:29:33 -0500

    ionic movil

--- www/js/services.js
+++ www/js/services.js
@@ -1,6 +1,6 @@
 angular.module('starter.services',['ngResource'])
 .factory('Session', function( $resource ){
 
-return $resource('http://localhost:5000/sessions/:sessionId');
+return $resource('https://ionicservernodejs.herokuapp.com/sessions/:sessionId');
 
 });
